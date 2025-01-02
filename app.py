ff7afced96d1df9e9c12d57bdc2e6c7fe4ff4856
MR-5OBOT <yassit7m7@gmail.com>
2025-01-02 11:37:30 +0100

    .

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ from formulas.position_size_futures import get_position_sizing_result_futures
 app = tk.Tk()
 app.title("Trading Toolbox")
 # app.geometry("500x400")
+app.resizable(False, False)
 
 # colors
 # style = ttk.Style()
-# style.config("TNotebook", background="#161616")
-# style.config("TNotebook.Tab", background="#101010", font=("Arial", 10))
-# style.config("TFrame", background="grey")
+# style.configure("TNotebook", background="#161616")
+# style.configure("TNotebook.Tab", background="#101010", font=("Arial", 10))
+# style.configure("TFrame", background="grey")
 
 # Create a Notebook
 notebook = ttk.Notebook(app, style="Horizonal.TNotebook") # Create a Notebook with vertical tabs
@@ -26,6 +27,7 @@ notebook.pack(expand=True, fill="both")
 
 # Apply a custom style for vertical tabs
 style = ttk.Style(app)
+style.theme_use("clam")
 style.configure("Vertical.TNotebook", tabposition='wn')  # west, wn
 style.configure("Vertical.TNotebook.Tab", width=20, padding=(10, 10))
 
@@ -50,34 +52,34 @@ def calculate_position_size_forex():
         pip_value = float(entry_pip_value_forex.get())
 
         result = get_position_sizing_result_forex(account_balance, risk_dollar, stop_loss_pips, pip_value)
-        label_result_futures_forex.config(text=result)
+        label_result_futures_forex.configure(text=result)
     
     except ValueError:
-        label_result_futures_forex.config(text="Error: Please enter valid numbers.")
+        label_result_futures_forex.configure(text="Error: Please enter valid numbers.")
 
 # position_size_forex GUI
-tk.Label(position_sizing_forex_tab, text="Position Sizing (Forex)", font=("Arial", 20)).pack(pady=20)
+ttk.Label(position_sizing_forex_tab, text="Position Sizing (Forex)", font=("Arial", 20)).pack(pady=20)
 
-tk.Label(position_sizing_forex_tab, text="Account Balance:").pack(pady=2)
-entry_account_balance_forex = tk.Entry(position_sizing_forex_tab)
+ttk.Label(position_sizing_forex_tab, text="Account Balance:").pack(pady=2)
+entry_account_balance_forex = ttk.Entry(position_sizing_forex_tab)
 entry_account_balance_forex.pack(pady=2)
 
-tk.Label(position_sizing_forex_tab, text="Dollar Risk $:").pack(pady=2)
-entry_amount_risk_forex = tk.Entry(position_sizing_forex_tab)
+ttk.Label(position_sizing_forex_tab, text="Dollar Risk $:").pack(pady=2)
+entry_amount_risk_forex = ttk.Entry(position_sizing_forex_tab)
 entry_amount_risk_forex.pack(pady=2)
 
-tk.Label(position_sizing_forex_tab, text="Stop Loss (pips):").pack(pady=2)
-entry_stop_loss_pips_forex = tk.Entry(position_sizing_forex_tab)
+ttk.Label(position_sizing_forex_tab, text="Stop Loss (pips):").pack(pady=2)
+entry_stop_loss_pips_forex = ttk.Entry(position_sizing_forex_tab)
 entry_stop_loss_pips_forex.pack(pady=2)
 
-tk.Label(position_sizing_forex_tab, text="Pip value: (eurusd=10$)").pack(pady=2)
-entry_pip_value_forex = tk.Entry(position_sizing_forex_tab)
+ttk.Label(position_sizing_forex_tab, text="Pip value: (eurusd=10$)").pack(pady=2)
+entry_pip_value_forex = ttk.Entry(position_sizing_forex_tab)
 entry_pip_value_forex.pack(pady=2)
 
-button_calculate = tk.Button(position_sizing_forex_tab, text="Calculate", command=calculate_position_size_forex)
+button_calculate = ttk.Button(position_sizing_forex_tab, text="Calculate", command=calculate_position_size_forex)
 button_calculate.pack(padx=5, pady=5)
 
-label_result_futures_forex = tk.Label(position_sizing_forex_tab)
+label_result_futures_forex = ttk.Label(position_sizing_forex_tab)
 label_result_futures_forex.pack(pady=2)
 
 
@@ -90,34 +92,34 @@ def calculate_position_size_futures():
         point_value = float(entry_point_value_futures.get())
 
         result = get_position_sizing_result_futures(account_balance, risk_dollar, stop_loss_points, point_value)
-        label_result_futures.config(text=result)
+        label_result_futures.configure(text=result)
 
     except ValueError:
-        label_result_futures.config(text="Error: Please enter valid numbers.")
+        label_result_futures.configure(text="Error: Please enter valid numbers.")
 
 # position_size_futures GUI
-tk.Label(position_sizing_futures_tab, text="Position Sizing (Futures)", font=("Arial", 20)).pack(pady=20)
+ttk.Label(position_sizing_futures_tab, text="Position Sizing (Futures)", font=("Arial", 20)).pack(pady=20)
 
-tk.Label(position_sizing_futures_tab, text="Account Balance:").pack(pady=2)
-entry_account_balance_futures = tk.Entry(position_sizing_futures_tab)
+ttk.Label(position_sizing_futures_tab, text="Account Balance:").pack(pady=2)
+entry_account_balance_futures = ttk.Entry(position_sizing_futures_tab)
 entry_account_balance_futures.pack(pady=2)
 
-tk.Label(position_sizing_futures_tab, text="Dollar Risk $:").pack(pady=2)
-entry_amount_risk_futures = tk.Entry(position_sizing_futures_tab)
+ttk.Label(position_sizing_futures_tab, text="Dollar Risk $:").pack(pady=2)
+entry_amount_risk_futures = ttk.Entry(position_sizing_futures_tab)
 entry_amount_risk_futures.pack(pady=2)
 
-tk.Label(position_sizing_futures_tab, text="Stop Loss (points):").pack(pady=2)
-entry_stop_loss_points_futures = tk.Entry(position_sizing_futures_tab)
+ttk.Label(position_sizing_futures_tab, text="Stop Loss (points):").pack(pady=2)
+entry_stop_loss_points_futures = ttk.Entry(position_sizing_futures_tab)
 entry_stop_loss_points_futures.pack(pady=2)
 
-tk.Label(position_sizing_futures_tab, text="P value: MNQ=2$ MES=5$").pack(pady=2)
-entry_point_value_futures = tk.Entry(position_sizing_futures_tab)
+ttk.Label(position_sizing_futures_tab, text="P value: MNQ=2$ MES=5$").pack(pady=2)
+entry_point_value_futures = ttk.Entry(position_sizing_futures_tab)
 entry_point_value_futures.pack(pady=2)
 
-button_calculate = tk.Button(position_sizing_futures_tab, text="Calculate", command=calculate_position_size_futures)
+button_calculate = ttk.Button(position_sizing_futures_tab, text="Calculate", command=calculate_position_size_futures)
 button_calculate.pack(padx=5, pady=5)
 
-label_result_futures = tk.Label(position_sizing_futures_tab)
+label_result_futures = ttk.Label(position_sizing_futures_tab)
 label_result_futures.pack(pady=2)
 
 
@@ -132,7 +134,7 @@ def probability_simulator():
 
         # Validate inputs
         if initial_balance <= 0 or risk_percent <= 0 or rr_ratio <= 0 or num_trades <= 0:
-            sim_label.config(text="Error: All inputs must be positive numbers.")
+            sim_label.configure(text="Error: All inputs must be positive numbers.")
 
         # Simulation logic
         balance = initial_balance
@@ -165,31 +167,31 @@ def probability_simulator():
         plt.show()
 
     except ValueError:
-        sim_label.config(text="Error: Please enter valid numbers.")
+        sim_label.configure(text="Error: Please enter valid numbers.")
 
 
 # probability_simulator GUI
-tk.Label(probability_sim_tab, text="Trading Simulator", font=("Arial", 24)).pack(pady=20)
+ttk.Label(probability_sim_tab, text="Trading Simulator", font=("Arial", 24)).pack(pady=20)
 
-tk.Label(probability_sim_tab, text="Initial Balance:").pack(pady=2)
-balance_entry = tk.Entry(probability_sim_tab)
+ttk.Label(probability_sim_tab, text="Initial Balance:").pack(pady=2)
+balance_entry = ttk.Entry(probability_sim_tab)
 balance_entry.pack(pady=5)
 
-tk.Label(probability_sim_tab, text="Risk Percentage:").pack(pady=2)
-risk_entry = tk.Entry(probability_sim_tab)
+ttk.Label(probability_sim_tab, text="Risk Percentage:").pack(pady=2)
+risk_entry = ttk.Entry(probability_sim_tab)
 risk_entry.pack(pady=5)
 
-tk.Label(probability_sim_tab, text="Risk-Reward Ratio:").pack(pady=2)
-rr_entry = tk.Entry(probability_sim_tab)
+ttk.Label(probability_sim_tab, text="Risk-Reward Ratio:").pack(pady=2)
+rr_entry = ttk.Entry(probability_sim_tab)
 rr_entry.pack(pady=5)
 
-tk.Label(probability_sim_tab, text="Number of Trades:").pack(pady=2)
-trades_entry = tk.Entry(probability_sim_tab)
+ttk.Label(probability_sim_tab, text="Number of Trades:").pack(pady=2)
+trades_entry = ttk.Entry(probability_sim_tab)
 trades_entry.pack(pady=5)
 
-tk.Button(probability_sim_tab, text="Run Simulation", command=probability_simulator).pack(pady=2)
+ttk.Button(probability_sim_tab, text="Run Simulation", command=probability_simulator).pack(pady=2)
 
-sim_label = tk.Label(probability_sim_tab, text="")
+sim_label = ttk.Label(probability_sim_tab, text="")
 sim_label.pack(pady=10)
 
 
@@ -201,26 +203,26 @@ def calculate_min_rr_required():
         rr_result = (1 - win_wate) / win_wate
         # Validate inputs
         if win_wate >= 1:
-            rr_result_label.config(text="Please enter a risk-reward ratio in decimal form.")
+            rr_result_label.configure(text="Please enter a risk-reward ratio in decimal form.")
         else:
-            messagebox.showinfo(title="R-R Required", message=(f"Min Risk-Reward required is: {rr_result:.2f}R"))
-            rr_result_label.config(text=(f"Min Risk-Reward Required is: {rr_result}"))
+            messagebox.showinfo(title="R-R Required", message=(f"Min Risk-Reward required is: {rr_result:.1f}R"))
+            rr_result_label.configure(text=(f"Min Risk-Reward Required is: {rr_result}"))
 
     except ValueError:
-        rr_result_label.config(text="Error: Please enter valid numbers.")
+        rr_result_label.configure(text="Error: Please enter valid numbers.")
 
 
-tk.Label(rr_required_tab, text='"Calculate the minimum Risk-Reward ratio required based on your system\'s Win-Rate:"', font=("Arial", 11)).pack(pady=10)
+ttk.Label(rr_required_tab, text='"Calculate the minimum Risk-Reward ratio required based on your system\'s Win-Rate:"', font=("Arial", 11)).pack(pady=10)
 
-rr_label = tk.Label(rr_required_tab, text="Win-Rate: (0.5 = 50%)")
+rr_label = ttk.Label(rr_required_tab, text="Win-Rate: (0.5 = 50%)")
 rr_label.pack(pady=5)
 
-win_rate_input = tk.Entry(rr_required_tab)
+win_rate_input = ttk.Entry(rr_required_tab)
 win_rate_input.pack(pady=5)
 
-tk.Button(rr_required_tab, text="Calculate", command=calculate_min_rr_required).pack(pady=5)
+ttk.Button(rr_required_tab, text="Calculate", command=calculate_min_rr_required).pack(pady=5)
 
-rr_result_label = tk.Label(rr_required_tab)
+rr_result_label = ttk.Label(rr_required_tab)
 rr_result_label.pack(pady=5)
 
 
