8c6110b3215015bda1256096e5ee9c6853595065
MR-5OBOT <115130443+MR-5OBOT@users.noreply.github.com>
2025-02-19 19:23:45 +0100

    Delete resources directory

--- resources/custom_func/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .min_winrate import min_winrate_calculator
-# from .position_size_forex import get_position_sizing_result_forex
-# from .position_size_futures import get_position_sizing_result_futures
-# from .probability_sim import simulate_trading
